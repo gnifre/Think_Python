e5951dc6957574e5c23392a9b69ea303c18dcd4c
marcello.fregni@gmail.com <marcello.fregni@gmail.com>
2022-09-24 11:41:12 +0200

    type function example

---
+++ typeFunctionExample.py
@@ -0,0 +1,21 @@
+# This is a sample Python script  to test the type function.
+
+# Press Maiusc+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def test_type_function():
+    # Use a breakpoint in the code line below to debug your script.
+    print("type(2) result: ", type(2))  # integer
+    print("type(42.0) result: ", type(42.0)) # float
+
+    print("type('Hello, World!') result: ", type('Hello, World!')) # sring
+    print("type('2') result: ", type('2'))  # sring
+    print("type('42.0') result: ", type('42.0')) # sring
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test_type_function()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
