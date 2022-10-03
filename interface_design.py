895898dadd6ab5131dcad5beb9792fea0ad1063e
marcello.fregni@gmail.com <marcello.fregni@gmail.com>
2022-10-03 19:48:04 +0200

    Return to 1988! using Turtle!

---
+++ interface_design.py
@@ -0,0 +1,22 @@
+import turtle
+
+
+# This is a sample Python script  to test the type function.
+
+# Press Maiusc+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def test_turtle():
+    # Use a breakpoint in the code line below to debug your script.
+    # Back to 1988!
+    little_turtle = turtle.Turtle()
+    print(little_turtle)
+    turtle.mainloop()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test_turtle()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
