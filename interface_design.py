9ba89fa3adda31a0432f941789264306e8b615ce
marcello.fregni@gmail.com <marcello.fregni@gmail.com>
2023-03-07 17:37:20 +0100

    interface design function with parameters

--- interface_design.py
+++ interface_design.py
@@ -12,13 +12,11 @@ def test_turtle():
     # Back to 1988!
     little_turtle = turtle.Turtle()
     print(little_turtle)
-    paint_square(little_turtle)
+
 
     # Another using for
     turtle.home()
-    for i in range(4):
-        little_turtle.rt(90)
-        little_turtle.fd(200)
+    polygon(current_turtle=little_turtle, steps=5, length=100)
 
     turtle.mainloop()
 
@@ -30,6 +28,14 @@ def paint_square(little_turtle):
     little_turtle.fd(100)
     little_turtle.lt(90)
     little_turtle.fd(100)
+
+def polygon(current_turtle, steps, length):
+    angle = 360/steps
+    for i in range(steps):
+        current_turtle.lt(angle)
+        current_turtle.fd(length)
+
+
 
 
 # Press the green button in the gutter to run the script.
